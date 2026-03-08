fa210795a5c782fd9e9019ffb6147594541fabc8
howardmann <howardmann27@gmail.com>
2026-03-08 16:22:03 +1100

    Typescript changes

---
+++ chapter-2/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log('Hello TypeScript!');
+let a = 1 + 2;
+let b = a * 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+let e = a + b;
+let API_KEY = "12123123123123";
+//# sourceMappingURL=index.js.map
